4e9a9823d4853155f9329f7a63017b1c1b709c89
Mubeen Nireman <mubeennireman@gmail.com>
2025-07-05 21:12:36 +0530

    create Armstrong function

--- pyPrectice.py
+++ pyPrectice.py
@@ -1,14 +1,29 @@
 # Python program to find the factorial of a number provided by user.abs
 
-userInput = int(input('Please enter a number..'))
+'''userInput = int(input('Please enter a number:'))
 factorial = 1
 if (userInput < 0):
     print("Sorry factorial is not exist for negative number.")
 else:
     for i in range (1, userInput + 1):
         factorial *= i
-    print(f'Factorial of {userInput} is {factorial}')
+    print(f'Factorial of {userInput} is {factorial}')'''
 
 
 
-# 
+# Write a program to print all Armstrong numbers in a given range. Note: An Armstrong number is a number whose sum of cubes of digits is equal to the number itself
+
+armInput = int(input('please enter a number:'))
+arm_remain = armInput
+length = len(str(armInput))
+sum = 0
+while arm_remain != 0:
+    reminder = arm_remain % 10
+    reminder **= length
+    sum += reminder
+    arm_remain //= 10
+if (sum == armInput):
+    print(f'{armInput} is a Armstrong Number')
+else:
+    print(f'{armInput} isn\'t a Armstrong Number')
+
