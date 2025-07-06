53f78bc86e5b81b091c58dc7ac39850ef7c68b6f
Mubeen Nireman <mubeennireman@gmail.com>
2025-07-06 16:02:21 +0530

    list with functionality of small, large and product of all items

--- pyPrectice.py
+++ pyPrectice.py
@@ -30,9 +30,31 @@ else:
 
 # write a program to multiply two numbers by repeated addition.abs
 
-Input_1 = int(input('Enter first number:'))
+'''Input_1 = int(input('Enter first number:'))
 Input_2 = int(input('Enter second number:'))
 sum = 0
 for i in range(1, Input_2 + 1):
     sum += Input_1
-print(f'Both numbers multiple is {sum}')
+print(f'Both numbers multiple is {sum}')'''
+
+
+
+#  write a program which takes list of numbers as input and finds:
+#  1. the large number in list      2. the smallest number in the list      3. product of all the item in the list
+
+'''list_ = []
+multiply_item = 1
+count_item = int(input('how many item you want in list? :'))
+if (count_item < 0):
+    print('invalid number!')
+elif (count_item == 0):
+    print('list doesn\'t have item')
+else:
+    for i in range(1, count_item + 1):
+        list_item = int(input('enter item:'))
+        multiply_item *= list_item
+        list_.append(list_item)
+    print(f'list  is:{list_}')
+    print(f'large number of list is :{max(list_)}')
+    print(f'small number of list is :{min(list_)}')
+    print(f'list item multiply is :{multiply_item}')'''
